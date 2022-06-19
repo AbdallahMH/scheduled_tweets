4e323c4d27ce0bf0cb301f6485a6b70dd98dfcb1
Abdallah Mohammed <sofrex8@gmail.com>
2022-06-19 14:40:36 +0300

    defining root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-#GEt /about
-get "about", to:"about#index"
+  #GEt /about
+  get "about", to:"about#index"
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/", to: "main#index"
+
 end
