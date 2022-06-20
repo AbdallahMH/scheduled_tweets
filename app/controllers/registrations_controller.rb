1fd16f3e2e142024e23e15aa50e13b59a08a27ed
Abdallah Mohammed <sofrex8@gmail.com>
2022-06-20 16:34:42 +0300

    sign up form

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,27 @@
+class RegistrationsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Sucessfully created account"
+    else
+      render :new
+    end
+    # params[:user]
+    # => {"email"=>"d@hello", "password"=>"password", "password_confirmation"=>"password"}
+    # params
+    #  Parameters: {"authenticity_token"=>"[FILTERED]", "user"=>{"email"=>"d@hello", "password"=>"[FILTERED]", "password_confirmation"=>"[FILTERED]"}, "commit"=>"Sign Up"}
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+end
