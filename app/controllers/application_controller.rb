cc7f930ae2d6e5c9e1f981154c4c3e5c7a86e0cd
abdallahmh <abdallahhezam1@gmail.com>
2022-11-04 21:07:23 +0300

    set time zone to Riyadh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-
   before_action :set_current_user
+  before_action :set_time_zone
   def set_current_user
     if session[:user_id]
       Current.user = User.find_by(id: session[:user_id])
@@ -11,5 +11,9 @@ class ApplicationController < ActionController::Base
     unless Current.user
       redirect_to sign_in_path, alert: "You must be logged in to do that"
     end
+  end
+
+  def set_time_zone
+    Time.zone = "Asia/Riyadh"
   end
 end
